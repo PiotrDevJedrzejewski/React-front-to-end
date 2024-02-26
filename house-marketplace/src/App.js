853ada169cb135597e4f4d45ff78b3156d880906
PiotrDevJedrzejewski <piotrdevjedrzejewski@gmail.com>
2024-02-26 17:20:07 +0100

    firebase

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,7 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
+import {
+  Explore,
+  ForgotPassword,
+  Offers,
+  Profile,
+  SignIn,
+  SignUp,
+} from './pages'
+
 function App() {
   return (
     <>
-      <h1>my app</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+      <ToastContainer />
     </>
   )
 }
