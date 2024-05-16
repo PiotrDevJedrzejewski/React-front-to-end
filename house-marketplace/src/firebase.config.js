39ff460eddf190580f7442f8c72b602feb0c884d
PiotrDevJedrzejewski <piotrdevjedrzejewski@gmail.com>
2024-05-16 14:08:51 +0200

    +env config

--- house-marketplace/src/firebase.config.js
+++ house-marketplace/src/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCryb3aFh66J2lcmssnnScijWLtooBeKJc',
-  authDomain: 'house-marketplace-app-332aa.firebaseapp.com',
-  projectId: 'house-marketplace-app-332aa',
-  storageBucket: 'house-marketplace-app-332aa.appspot.com',
-  messagingSenderId: '85443613224',
-  appId: '1:85443613224:web:70afa88b5eaa9d5fef5b1d',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 // Initialize Firebase
