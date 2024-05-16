6538aa828eaef4290aea8bc8c847e22540212571
PiotrDevJedrzejewski <99095779+PiotrDevJedrzejewski@users.noreply.github.com>
2024-05-16 13:55:14 +0200

    Update firebase.config.js
    -key

--- house-marketplace/src/firebase.config.js
+++ house-marketplace/src/firebase.config.js
@@ -3,12 +3,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCryb3aFh66J2lcmssnnScijWLtooBeKJc',
-  authDomain: 'house-marketplace-app-332aa.firebaseapp.com',
-  projectId: 'house-marketplace-app-332aa',
-  storageBucket: 'house-marketplace-app-332aa.appspot.com',
-  messagingSenderId: '85443613224',
-  appId: '1:85443613224:web:70afa88b5eaa9d5fef5b1d',
+
 }
 
 // Initialize Firebase
